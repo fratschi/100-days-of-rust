cdc2762bc58ac1782f279a8df856fe276e033e66
Frank Ratschinski <frank.ratschinski@prodyna.com>
2023-02-22 18:53:33 +0100

    day-009

---
+++ rust-009/src/main.rs
@@ -0,0 +1,33 @@
+use serde_derive::Deserialize;
+use serde_derive::Serialize;
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Person {
+    pub userid: i64,
+    pub name: String,
+    #[serde(rename = "given_name")]
+    pub given_name: String,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_add_ok() {
+        let data = r#"{
+            "userid": 1234,
+            "name": "Doe",
+            "given_name": "John"
+        }"#;
+
+        let person: Person = serde_json::from_str(data).expect("JSON was not well-formatted");
+        println!("{:?}", person)
+    }
+}
